422a0e8a677ec1f2435236a87ef384c825e333d2
Josmer Martinez <Josmer Martinez@NT194S>
2021-08-11 09:55:01 -0400

    prueba

--- WebApiAutores/Entidades/Libro.cs
+++ WebApiAutores/Entidades/Libro.cs
@@ -12,11 +12,11 @@ namespace WebApiAutores.Entidades
         public int Id { get; set; }
         [Required]
         [PrimerLetraMayuscula]
-        [StringLength(maximumLength: 250)]
+        [StringLength(maximumLength: 300)]
         public string Titulo { get; set; }
         public DateTime? FechaPublicacion { get; set; }
         public List<Comentario> Comentarios { get; set; }
         public List<AutorLibro> AutoresLibros { get; set; }
-
+        
     }
 }
